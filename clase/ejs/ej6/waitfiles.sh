fa6bf995d01ea936604438b9aa224913fd50de0e
amartii <amartinez.tellez@gmail.com>
2025-12-03 13:13:57 +0100

    maitfiles.sh terminado

--- clase/ejs/ej6/waitfiles.sh
+++ clase/ejs/ej6/waitfiles.sh
@@ -6,14 +6,32 @@ then
 	exit 1
 fi
 
-//comprobar esto: que no funciona
-if test "$@" -f
-then
-	echo error: is not a regular file >&2
-	exit 1
-fi
+for f in "$@"
+do
+	if test ! -f "$f"
+	then
+		echo error: $f is not a regular file >&2
+		exit 1
+	fi
+done
 
-echo hace cosas
+queda_algo(){
+	for f in "$@"
+	do 
+		if test -f "$f"
+		then 
+			return 0
+		fi
+	done
+	return 1
+}
+
+#polling cada segundo
+while queda_algo "$@"
+do 
+	sleep 1
+done
+echo done
 
 exit 0
 	
