5ef0e9b74999c5fbafe9ea8280cc322f575cf540
amartii <amartinez.tellez@gmail.com>
2025-12-16 15:47:12 +0100

    ej7 finalizado

--- clase/ejs/ej7/catlen.sh
+++ clase/ejs/ej7/catlen.sh
@@ -1,3 +1,39 @@
 #!/bin/sh
-echo hola
+usage(){
+    echo usage: ./catlen.sh dir >&2
+    exit 1
+}
+
+borrar_outputs(){
+    rm -f "$1"/*.output 2>/dev/null
+}
+
+listar_txt(){
+    (cd "$1" && ls *.txt 2>/dev/null | sort)
+}
+
+concatenar_txt(){
+    listar_txt "$1" | while read fichero
+    do 
+        longitud=$(printf "%s\n" "$fichero" |wc -c)
+        longitud=$((longitud-1))
+        output="$longitud.output"
+        cat "$fichero" >> "$output"
+    done
+}
+
+if test $# -ne 1
+then 
+   usage
+fi
+
+if test ! -d "$1"
+then
+    echo error: $1 not a directory >&2
+    usage
+    
+fi
+borrar_outputs "$1"
+concatenar_txt "$1"
+
 exit 0
